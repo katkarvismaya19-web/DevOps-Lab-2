a264eae406c2ed41518d0499e605b4025c852b7c
katkarvismaya19-web <katkarvismaya19@gmail.com>
2026-08-09 00:07:30 +0530

    Create AppTest.java

---
+++ src/main/java/com/vit/demo/src/test/java/com/vit/demo/src/test/java/com/vit/demo/AppTest.java
@@ -0,0 +1,12 @@
+package com.vit.demo;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
